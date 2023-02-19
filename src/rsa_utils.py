d68306635dc38f68e73a10c4a2bde9132cd9906a
petri-98 <gtringali4@gmail.com>
2023-02-19 21:51:04 +0100

    chore(src/rsa_utils.py): used conventions

--- src/rsa_utils.py
+++ src/rsa_utils.py
@@ -1,9 +1,12 @@
 import random
 
-from .math_utils import euclidean_algo, extended_GCD, fast_power, find_prime
+from .math_utils import euclidean_algo, extended_gcd, fast_power, find_prime
 
 
-def generate_RSA_Key(b):
+# TODO add docstrings
+
+
+def generate_rsa_key(b):
     p = find_prime(2**(b - 1), 2**b - 1)
     q = find_prime(2**(b - 1), 2**b - 1)
 
@@ -13,17 +16,17 @@ def generate_RSA_Key(b):
 
     g = euclidean_algo(p - 1, q - 1)
     m = (p - 1) * (q - 1) // g
-    d = extended_GCD(e, m)[1] % m
+    d = extended_gcd(e, m)[1] % m
 
-    PublicKey = [p * q, e]
-    PrivateKey = [p * q, d]
+    public_key = [p * q, e]
+    private_key = [p * q, d]
 
-    return [PublicKey, PrivateKey]
-
-
-def RSA_encrypt(message, PublicKey):
-    return fast_power(message, PublicKey[1], PublicKey[0])
+    return [public_key, private_key]
 
 
-def RSA_decrypt(cipher, PrivateKey):
-    return fast_power(cipher, PrivateKey[1], PrivateKey[0])
+def rsa_encrypt(message, public_key):
+    return fast_power(message, public_key[1], public_key[0])
+
+
+def RSA_decrypt(cipher, private_key):
+    return fast_power(cipher, private_key[1], private_key[0])
