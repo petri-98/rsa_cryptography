ea83f3c221c50b4bc4f5575640d705524aa308c2
petri-98 <gtringali4@gmail.com>
2023-02-20 01:57:29 +0100

    chore(src/rsa_utils.py): minor adjustments

--- src/rsa_utils.py
+++ src/rsa_utils.py
@@ -1,6 +1,6 @@
 import random
 
-from .math_utils import euclidean_algo, extended_gcd, fast_power, find_prime
+from .math_utils import extended_gcd, fast_power, find_prime
 
 
 # TODO add docstrings
@@ -11,10 +11,10 @@ def generate_rsa_key(b):
     q = find_prime(2**(b - 1), 2**b - 1)
 
     e = 2
-    while euclidean_algo(e, (p - 1) * (q - 1)) != 1:
+    while extended_gcd(e, (p - 1) * (q - 1))[0] != 1:
         e = random.randrange(2, (p - 1) * (q - 1) - 1)
 
-    g = euclidean_algo(p - 1, q - 1)
+    g = extended_gcd(p - 1, q - 1)[0]
     m = (p - 1) * (q - 1) // g
     d = extended_gcd(e, m)[1] % m
 
@@ -24,9 +24,9 @@ def generate_rsa_key(b):
     return [public_key, private_key]
 
 
-def rsa_encrypt(message, public_key):
-    return fast_power(message, public_key[1], public_key[0])
+def rsa_encrypt(text, public_key):
+    return fast_power(text, public_key[1], public_key[0])
 
 
-def rsa_decrypt(cipher, private_key):
-    return fast_power(cipher, private_key[1], private_key[0])
+def rsa_decrypt(text, private_key):
+    return fast_power(text, private_key[1], private_key[0])
